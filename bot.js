cd0a7324230b5630f15ce22c030f86800052d77b
Jenell Pizarro <nellarro@gmail.com>
2016-12-11 19:35:02 -0500

    commit before pull

--- bot.js
+++ bot.js
@@ -1,2 +1,16 @@
 var twit = require('twit')
 var config = require('./config.js')
+
+var retweet = () => {
+  var params = {
+    q: '#node', '#NodeJS',
+    result_type: 'recent',
+    lang: 'en'
+  }
+}
+
+Twitter.get('search/tweets', params, function(error, data)) {
+  if (!error) {
+    let retweetId = data
+  }
+}
