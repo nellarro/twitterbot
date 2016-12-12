135a28b1c3fedd09b96654d08c0f32d4182a2718
Brian Gates <brian.gates@theironyard.com>
2016-12-11 19:44:26 -0500

    write retweet function

--- bot.js
+++ bot.js
@@ -1,4 +1,31 @@
 var Twit = require('twit')
 var config = require('./config.js')
 
-var twitter = new Twit(config)
+var Twitter = new Twit(config)
+
+var retweet = function() {
+  var params = {
+    q: '#nodejs, #Nodejs',
+    result_type: 'recent',
+    lang: 'en'
+  }
+
+  Twitter.get('search/tweets', params, (error, data) => {
+    if (error) {
+      console.log('something went wrong while searching')
+    } else {
+      var retweetId = data.statuses[0].id_str
+
+      Twitter.post('statuses/retweet/:id', {
+        id: retweetId
+      }, (error, response) => {
+        if (response) {
+          console.log('retweeted!')
+        } else {
+          console.log('something went wrong while retweeting')
+        }
+      })
+    }
+  })
+  retweet()
+  setInterval(retweet, 50000)
