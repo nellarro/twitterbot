1c847acf7bb797452309dc8ee2698fb827051ba4
Jenell Pizarro <nellarro@gmail.com>
2016-12-11 19:51:22 -0500

    all but setInterval() is added

--- bot.js
+++ bot.js
@@ -4,14 +4,26 @@ var twitter = new Twit(config)
 
 var retweet = () => {
   var params = {
-    q: '#node', '#NodeJS',
+    q: '#JavaScript30', '#NodeJS',
     result_type: 'recent',
     lang: 'en'
   }
 }
 
-Twitter.get('search/tweets', params, function(error, data)) {
-  if (!error) {
-    let retweetId = data
+Twitter.get('search/tweets', params, function(err, data)) {
+  if (!err) {
+    let retweetId = data.status[0].id_str; // tells TWITTER to retweet
+    Twitter.post('statuses/retweet/:id', {
+      id:retweetID
+    }, function(err, response) {
+      if (response) {
+        console.log('💛')
+      }
+      if (err) {
+        console.log('Something wicked this way comes! 💀🐍! Or maybe just duplication.')
+      }
+    })
+  } else {
+    console.log('Something went wrong while SEARCHING...')
   }
 }
