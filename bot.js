7baa1153bfc32ed0f6e138e4e5df3a3e526ef523
Jenell Pizarro <nellarro@gmail.com>
2016-12-11 19:41:04 -0500

    fixed merge conflict

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var Twit = require('twit')
 var config = require('./config.js')
+var twitter = new Twit(config)
 
-<<<<<<< HEAD
 var retweet = () => {
   var params = {
     q: '#node', '#NodeJS',
@@ -15,6 +15,3 @@ Twitter.get('search/tweets', params, function(error, data)) {
     let retweetId = data
   }
 }
-=======
-var twitter = new Twit(config)
->>>>>>> dd24b629800e629f2d7a344ead86bfac88967f7b
